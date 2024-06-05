28c15d8862190e8afe69ffb99d3cb86176dd1452
Hasankhan170 <hasankhanali0316@gmail.com>
2024-06-05 09:20:11 -0700

    login page

--- register.js
+++ register.js
@@ -1,9 +1,37 @@
 
-const form = document.querySelector('form')
-const userName = document.querySelector('#register-user')
-const email = document.querySelector('#register-email')
-const password = document.querySelector('#register-password')
-const againPassword = document.querySelector('#register-again-password')
+const form = document.querySelector('form');
+const userName = document.querySelector('#register-user');
+const email = document.querySelector('#register-email');
+const password = document.querySelector('#register-password');
+const againPassword = document.querySelector('#register-again-password');
+
+
+const nameRegex = /^[A-Z][a-zA-Z0-9]{4,13}(?: [A-Z][a-zA-Z0-9]*)?$/;
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const passwordRegex = /^(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/;
+
+function validateName(name){
+    return nameRegex.test(name);
+}
+
+function validateEmail(email){
+    return emailRegex.test(email);
+}
+
+function validatePassword(password){
+    return passwordRegex.test(password);
+}
+
+function validateAgainPassword(password,againPassword){
+    return password === againPassword
+}
+
+
+
+
+
+
+
 
 
 function addData(){
@@ -19,10 +47,10 @@ form.addEventListener('submit', function(event){
     arr.push({
         userName: userName.value,
         email: email.value,
-        password:  password.value,
-        againPassword:  againPassword.value
+        password: password.value,
+        againPassword: againPassword.value
     })
     addData();
-})
+});
 
 
