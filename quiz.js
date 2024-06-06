ca065b7630f3b60a619a81b9aa39d7390b391973
Hasankhan170 <hasankhanali0316@gmail.com>
2024-06-06 04:04:50 -0700

    login page

---
+++ quiz.js
@@ -0,0 +1,22 @@
+
+// const div = document.querySelector('div')
+
+// axios(' https://opentdb.com/api_config.javascript')
+// .then((res)=>{
+//     console.log(res.data)
+//     res.data.map((item) => {
+//         div.innerHTML += `
+//         <h1 style="color: black;">${item.category}</h1>
+
+//         `;
+//     })
+// }).catch((error)=>{
+//     console.log(error)
+// })
+
+
+
+
+
+
+
