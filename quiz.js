19ab54165a11423c52f612714fad97a45a64c933
Hasankhan170 <hasankhanali0316@gmail.com>
2024-06-08 02:04:42 -0700

    quiz app

--- quiz.js
+++ quiz.js
@@ -1,21 +1,42 @@
 
-// const div = document.querySelector('div')
+const div = document.querySelector('div')
 
-// axios(' https://opentdb.com/api_config.javascript')
-// .then((res)=>{
-//     console.log(res.data)
+// const getQusetion = async ()=>{
+// try {
+//     const res = await axios('https://the-trivia-api.com/v2/questions')
 //     res.data.map((item) => {
 //         div.innerHTML += `
-//         <h1 style="color: black;">${item.category}</h1>
-
-//         `;
-//     })
-// }).catch((error)=>{
+//         <h1>${item.question}</h1>
+//         <p style="color: black;">${item.incorrectAnswers}</p>`
+//     });
+// } catch (error) {
 //     console.log(error)
-// })
+    
+// }
+//     console.log(res.data)
+// }
+// getQusetion()
 
+// const div = document.querySelector('#quiz-container');
 
+const getQuestions = async () => {
+    try {
+        const res = await axios('https://the-trivia-api.com/v2/questions');
+        res.data.forEach((item) => {
+            div.innerHTML += `
+                <h1>${item.question.text}</h1>
+                <ul>
+                    ${item.incorrectAnswers.map(answer => `<li style="color: black;">${answer}</li>`).join('')}
+                </ul>
+            `;
+        });
+        console.log(res.data);
+    } catch (error) {
+        console.error('Error fetching the questions:', error);
+    }
+};
 
+getQuestions();
 
 
 
