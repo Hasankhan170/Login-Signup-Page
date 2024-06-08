bcb7f26eb54c4a5b9dab995492d6219aff03a1b0
Hasankhan170 <hasankhanali0316@gmail.com>
2024-06-07 22:30:55 -0700

    login page

--- Login.js
+++ Login.js
@@ -1,17 +1,31 @@
-const email = localStorage.getItem('email')
-const password = localStorage.getItem('password')
+const emailLogin = document.querySelector('#email-login');
+const passwordLogin = document.querySelector('#password-login');
 const formLogin = document.querySelector('form');
+
+
+function getUsers(){
+    return JSON.parse(localStorage.getItem('users')) || []
+}
+
+function validateLogin(email,password){
+    const users = getUsers();
+    return users.find(user =>user.email === email && user.password === password )
+}
 
 formLogin.addEventListener('submit', function(event){
     event.preventDefault();
-    if (email === null || password === null) {
+    const email = emailLogin.value;
+    const password = passwordLogin.value;
+
+       const users = getUsers()
+    if (users.length === 0) {
         alert('Please register before attempting to login.');
         return
     }
-    const emailLogin = document.querySelector('#email-login').value;
-    const passwordLogin = document.querySelector('#password-login').value;
 
-    if(emailLogin === email && passwordLogin === password ){
+    const user = validateLogin(email,password)
+
+    if(user ){
         alert('Login Successful')
         window.location = 'quiz.html'
     }else{
@@ -19,4 +33,6 @@ formLogin.addEventListener('submit', function(event){
     }
     formLogin.reset()
 })
+
+
 
